369124465658480cbb17b8ce20383a7d103da2c6
Himanshu Kumar <himanshukr.32@gmail.com>
2018-06-27 20:10:12 +0530

    Update Basic_functions.ino

--- Odometry_rk19/Basic_functions.ino
+++ Odometry_rk19/Basic_functions.ino
@@ -1,6 +1,6 @@
 float dist(float x1,float y1,float x2,float y2)
 {
-  return sqrt((x1-x2)*(x1-x2)+(y1-y1)*(y1-y2));
+  return sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
 }
 
 float angle(float x1,float y1,float x2,float y2)
